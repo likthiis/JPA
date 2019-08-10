088ca56c225b5fb6ce9d9eaf273a392aa32c51d1
John Smith <1501681615@qq.com>
2019-08-10 16:16:10 +0800

    20190810commit:JPA Set.

---
+++ src/main/java/com/test/demo/services/DemoService.java
@@ -0,0 +1,45 @@
+package com.test.demo.services;
+
+import com.test.demo.domain.entities.Address;
+import com.test.demo.domain.entities.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    /**
+     * 根据名称查找地区
+     * @param name
+     * @return
+     */
+    public List<Address> queryAddress(String name){
+        return addressRepository.queryListByName(name);
+    }
+
+
+    public Address findById(int id){
+//        Address address = new Address();
+//        address.setAddress("上海新国际博览中心");
+//
+//        return address;
+
+        return addressRepository.findOne(id);
+    }
+
+    public Address updateName(int id, String address){
+        Address a = addressRepository.findOne(id);
+        if (a!=null){
+            a.setAddress(address);
+            addressRepository.save(a);
+            return a;
+        }else
+            return null;
+    }
+
+}
