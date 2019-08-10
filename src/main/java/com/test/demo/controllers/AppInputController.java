739d326df05098afd11d005fd61b93713d1f7fe3
John Smith <1501681615@qq.com>
2019-08-10 20:49:04 +0800

    20190810commit:mqtt and appInput controller built .

---
+++ src/main/java/com/test/demo/controllers/AppInputController.java
@@ -0,0 +1,9 @@
+package com.test.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app_input")
+public class AppInputController {
+}
